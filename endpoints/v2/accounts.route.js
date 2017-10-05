681efbfc4d509deec060f23c5d97697f55db5f45
garrett <garrett@highwaythreesolutions.com>
2017-10-04 17:19:21 -0700

    Udpate accounts routeing

--- endpoints/v2/accounts.route.js
+++ endpoints/v2/accounts.route.js
@@ -4,11 +4,11 @@ var service=require("../../services");
 // console.log(validation.user.login)
 module.exports = function (end_b, app, controller, authenticate, services, validations) {
 
-  app.get(    end_b+'/accounts', validation.jwt.check, controller.accounts.get);
-  app.post(   end_b+'/accounts', validation.jwt.check, controller.accounts.add);
   app.post(   end_b+'/accounts/:id/update', validation.jwt.check, controller.accounts.update);
-  app.get(    end_b+'/accounts/:id', validation.jwt.check, controller.accounts.get);
   app.delete( end_b+'/accounts/:id/remove', validation.jwt.check, controller.accounts.remove);
   app.get(    end_b+'/accounts/:id/projects', validation.jwt.check, controller.accounts.get_projects);
+  app.get(    end_b+'/accounts/:id', validation.jwt.check, controller.accounts.get);
+  app.get(    end_b+'/accounts', validation.jwt.check, controller.accounts.get);
+  app.post(   end_b+'/accounts', validation.jwt.check, controller.accounts.add);
 
 }
